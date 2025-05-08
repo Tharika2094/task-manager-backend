eae87c3f4c6b4464c29d32282dce75fb66f2fdda
Tharika2094 <tvijayangi@gmail.com>
2025-05-08 10:46:45 +0100

    Update CORS settings for frontend deployed URL

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:["http://localhost:3000/"]
+    origin:["http://localhost:3000/","https://mern-task-app-glom.onrender.com"]
 }));
 app.use("/api/tasks",router);
 
